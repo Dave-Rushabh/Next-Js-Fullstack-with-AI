d0de9c2317a556c0c3e504986b326fa3ed55ec74
Dave-Rushabh <rushabh.dave.dev@gmail.com>
2024-12-17 15:19:09 +0530

    Message sending API done

---
+++ src/app/api/get-messages/route.ts
@@ -0,0 +1,66 @@
+import { getServerSession, User } from "next-auth";
+import { authOptions } from "../auth/[...nextauth]/options";
+import dbConnect from "@/lib/dbConnect";
+import UserModel from "@/models/User";
+import mongoose from "mongoose";
+
+export async function GET(request: Request) {
+  await dbConnect();
+
+  const session = await getServerSession(authOptions);
+  const user = session?.user;
+
+  if (!session || !user) {
+    return Response.json(
+      {
+        success: false,
+        message: "Not authenticated",
+      },
+      {
+        status: 401,
+      }
+    );
+  }
+  const userId = new mongoose.Types.ObjectId(user._id?.toString());
+
+  try {
+    const user = await UserModel.aggregate([
+      { $match: { id: userId } },
+      { $unwind: "$messages" },
+      { $sort: { "messages.createdAt": -1 } },
+      { $group: { _id: "$_id", messages: { $push: "$messages" } } },
+    ]);
+
+    if (!user || user.length === 0) {
+      return Response.json(
+        {
+          success: false,
+          message: "User not found",
+        },
+        {
+          status: 401,
+        }
+      );
+    }
+
+    return Response.json(
+      {
+        success: true,
+        messages: user[0].messages,
+      },
+      {
+        status: 200,
+      }
+    );
+  } catch (error) {
+    return Response.json(
+      {
+        success: false,
+        message: "Failed to load the messages",
+      },
+      {
+        status: 500,
+      }
+    );
+  }
+}
---
+++ src/app/api/send-message/route.ts
@@ -0,0 +1,67 @@
+import dbConnect from "@/lib/dbConnect";
+import UserModel from "@/models/User";
+import { Message } from "@/models/User";
+
+export const POST = async (request: Request) => {
+  await dbConnect();
+
+  const { username, content } = await request.json();
+
+  try {
+    const foundUser = await UserModel.findOne({ username });
+
+    if (!foundUser) {
+      return Response.json(
+        {
+          success: false,
+          message: "User not found to send the message",
+        },
+        {
+          status: 500,
+        }
+      );
+    }
+
+    const isAcceptingMessage = foundUser.isAcceptingMessages;
+
+    if (!isAcceptingMessage) {
+      return Response.json(
+        {
+          success: false,
+          message:
+            "The user is not accepting the new messages; your message could not be sent",
+        },
+        {
+          status: 403,
+        }
+      );
+    }
+
+    const newMessage = { content, createdAt: new Date() };
+
+    foundUser.messages.push(newMessage as Message);
+
+    await foundUser.save();
+
+    return Response.json(
+      {
+        success: true,
+        message: "Message sent successfully !",
+      },
+      {
+        status: 403,
+      }
+    );
+  } catch (error) {
+    console.error(error, "Failed to send the message");
+    return Response.json(
+      {
+        success: false,
+        message: "Failed to send the message",
+      },
+      {
+        status: 500,
+      }
+    );
+  }
+};
